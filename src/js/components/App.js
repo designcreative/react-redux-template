71224fc747d19fb734ed46abf8d99cf398926d33
Alexander Rizhikoff <designcreative.me@gmail.com>
2017-08-26 02:43:20 +0300

    Changed App to standard react

--- src/js/components/App.js
+++ src/js/components/App.js
@@ -1,16 +1,20 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ItemList from '../containers/item-list';
 import ItemDetail from '../containers/item-detail';
 require('../../scss/main.scss');
 
-const App = () => {
-  <div>
-    <h2>Item List</h2>
-    <ItemList />
-    <hr/>
-    <h2>Item Detail</h2>
-    <ItemDetail />
-  </div>
-};
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <h2>Item List</h2>
+        <ItemList />
+        <hr/>
+        <h2>Item Detail</h2>
+        <ItemDetail />
+      </div>
+    );
+  }
+}
 
 export default App;
